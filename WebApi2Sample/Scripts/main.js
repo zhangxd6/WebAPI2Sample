b7dbb0adbfa67f015d86da756d55fe33f87ad225
xzhang <xzhang@meridium.com>
2013-12-16 15:17:03 -0500

    setup require config and main

---
+++ WebApi2Sample/Scripts/main.js
@@ -0,0 +1,57 @@
+﻿/// <reference path="lib/require.js" />
+
+/*global requirejs: true, define: true */
+requirejs.config({
+    paths: {
+        'vm': 'viewmodels',
+        'text': 'lib/require/text',
+        'knockout': 'lib/knockout-3.0.0.min',
+        'ko_es5': 'lib/knockout-es5.min',
+        'ko_punches': 'lib/knockout.punches',
+        'jquery': 'lib/jquery-2.0.3.min',
+        'toastr': 'lib/toastr',
+        'underscore': 'lib/underscore'
+    },
+    shim: {
+        'ko_es5': {
+            deps: ['knockout']
+        },
+        'ko_punches': {
+            deps: ['knockout']
+        },
+        'knockout': {
+            exports: 'ko'
+        },
+        'jquery': {
+            exports: '$'
+        },
+        'toastr': {
+            exports: 'toastr'
+        },
+        'underscore': {
+            exports: '_'
+        }
+    }
+    //urlArgs: "ver=" + window.appSettings.productVersion +
+    //    "." + window.appSettings.majorVersion +
+    //    "." + window.appSettings.minorVersion +
+    //    "." + window.appSettings.build
+});
+
+define(function (require) {
+    'use strict';
+
+    var ko = require('knockout'),
+        ProductsViewModel = require('vm/products'),
+        toastr = require('toastr');
+
+    require('ko_punches');
+
+    ko.punches.interpolationMarkup.enable();
+    toastr.options.newestOnTop = false;
+
+    var productsViewModel = new ProductsViewModel();
+    productsViewModel.getAllProducts();
+
+    ko.applyBindings(productsViewModel);
+});
